8f9c69f32763ec4cead7bbe330319f1480687882
Unnati <unnatixlr8@gmail.com>
2018-11-01 15:13:37 +0530

    added default value in marks

--- student/models.py
+++ student/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 class Students(models.Model):
@@ -8,8 +9,8 @@ class Students(models.Model):
 	USN = models.IntegerField(primary_key=True)
 	Name = models.CharField(max_length=20)
 	Department = models.CharField(max_length=4)
-	Email = models.EmailField(null=True)
-	Phone = models.CharField(max_length = 15,null=True)
+	Email = models.EmailField(null=True, blank=True)
+	Phone = models.CharField(max_length = 15,null=True, blank=True)
 
 	def __str__(self):
 		return str(self.USN)
@@ -20,12 +21,12 @@ class Marks(models.Model):
 		verbose_name_plural = "Marks"
 	
 	USN = models.OneToOneField(Students, on_delete=models.CASCADE)
-	MAT = models.IntegerField(null=True)
-	CHE = models.IntegerField(null=True)
-	PCD = models.IntegerField(null=True)
-	CED = models.IntegerField(null=True)
-	ELN = models.IntegerField(null=True)
-	CIV = models.IntegerField(null=True)
+	MAT = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+	CHE = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+	PCD = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+	CED = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+	ELN = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+	CIV = models.IntegerField(blank=True, default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
 
 
 	def __str__(self):
