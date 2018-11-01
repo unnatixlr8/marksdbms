b1a400b0f8fdf66efbb849cce4c4a1ed6a3ff4a3
Unnati <unnatixlr8@gmail.com>
2018-11-02 00:05:23 +0530

    added departs

--- student/admin.py
+++ student/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from student.models import Students
 from student.models import Marks
+from student.models import Depart
 
 # Register your models here.
 
 class StudentsAdmin(admin.ModelAdmin):
-	list_display = ('USN', 'Name', 'Department', 'Email', 'Phone')
+	list_display = ('USN', 'Name','Department','Email', 'Phone')
 	search_fields = ['USN', 'Name','Email','Phone','Department']
 
 
@@ -14,6 +15,11 @@ class MarksAdmin(admin.ModelAdmin):
 	search_fields = ['USN__USN']
 
 
+class DepartAdmin(admin.ModelAdmin):
+	list_display = ('DCode','DName')
+
+
 admin.site.register(Students,StudentsAdmin)
 admin.site.register(Marks,MarksAdmin)
+admin.site.register(Depart,DepartAdmin)
 admin.site.site_header = 'MarkX Admin Panel'
